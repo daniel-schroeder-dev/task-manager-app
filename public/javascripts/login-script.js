8a3f9ab6ad6107d2b74c5079fb1fb548a57017cb
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-14 14:56:37 -0600

    Style login page, add a little JS to form to have active styles on focus.

---
+++ public/javascripts/login-script.js
@@ -0,0 +1,10 @@
+let lastActive = {};
+document.addEventListener('focusin', function(e) {
+  if (e.target.type === 'submit') return;
+  lastActive = e.target.previousElementSibling.firstChild;
+  lastActive.classList.add('active');
+});
+
+document.addEventListener('focusout', function(e) {
+  lastActive.classList.remove('active');
+});
