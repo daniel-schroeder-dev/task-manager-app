f9a516aab894f97a7d1cbd60dfecab35cbd31b63
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-16 11:32:09 -0600

    Remove cookieParser middleware from app.js.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -21,7 +20,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
