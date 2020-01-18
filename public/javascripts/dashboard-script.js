7689b6b8430861036cfaab6ca553b3efe00a1222
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-18 00:17:58 -0600

    Add styles for new lists, setup JS style adjustment for before the 10th of the month.

--- public/javascripts/dashboard-script.js
+++ public/javascripts/dashboard-script.js
@@ -1,3 +1,8 @@
 const dateSpan = document.querySelector('#date');
 
-dateSpan.textContent = new Date().getDate();
+const todaysDate = new Date().getDate();
+dateSpan.textContent = todaysDate;
+
+if (todaysDate < 10) {
+  dateSpan.style.marginLeft = '2px';
+}
