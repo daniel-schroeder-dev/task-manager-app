aa42559d6709a40203d589ae57d82f5cffce8d44
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-17 21:40:06 -0600

    Create an authTokens property on user schema and User.generateAuthToken method.

--- src/models/user.js
+++ src/models/user.js
@@ -7,15 +7,21 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   authToken: String,
+  authTokens: Array,
 });
 
 userSchema.statics.create = async function(userData) {
   const saltRounds = 10;
   const user = new this(userData);
   user.password = await bcrypt.hash(user.password, saltRounds);
-  user.authToken = await jwt.sign({ _id: user._id }, 'secret', { expiresIn: '7d'});
+  await user.generateAuthToken();
   await user.save();
   return user;
+};
+
+userSchema.methods.generateAuthToken = async function() {
+  this.authToken = await jwt.sign({ _id: this._id }, 'secret', { expiresIn: '7d'});
+  this.authTokens.push(this.authToken);
 };
 
 module.exports = mongoose.model('User', userSchema);
