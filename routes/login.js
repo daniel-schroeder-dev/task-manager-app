a12c79633386476374e23e915471b06f35d9f6f4
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-18 11:06:09 -0600

    Setup basic login error handling and ensure passwords match at login.

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const User = require('../src/models/user');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('login');
+  res.render('login', { err: '' });
 });
 
 router.post('/', async (req, res, next) => {
   const [ user ] = await User.find({ email: req.body.email });
+  if (!user) return res.status(401).render('login', { err: 'Invalid email' });
+  const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+  if (!passwordMatch) return res.status(401).render('login', { err: 'Invalid password' });
   await user.generateAuthToken();
   await user.save();
   res.cookie('jwt', user.authToken, {
