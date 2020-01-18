5f1d8616a577d107ffa2df2d5e47a054d26a5f88
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-18 11:30:25 -0600

    Ensure jwt cookie exists in auth middleware.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports = async (req, res, next) => {
+  if (!req.cookies.jwt) return res.redirect('/login');
   const _id = await jwt.verify(req.cookies.jwt, process.env.JWT_SECRET)._id;
   req.user = await User.findById(_id);
   next();
