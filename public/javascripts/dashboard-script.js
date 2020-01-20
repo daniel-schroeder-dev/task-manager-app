d22fcc25ef485acc1729ad52a8adbc24efff9ec0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-20 15:27:46 -0600

    Trigger popup animation on click for addNewList functionality.

--- public/javascripts/dashboard-script.js
+++ public/javascripts/dashboard-script.js
@@ -1,4 +1,4 @@
-const dateSpan = document.querySelector('#date');
+const dateSpan = document.getElementById('date');
 
 const todaysDate = new Date().getDate();
 dateSpan.textContent = todaysDate;
@@ -6,3 +6,11 @@ dateSpan.textContent = todaysDate;
 if (todaysDate < 10) {
   dateSpan.style.marginLeft = '2px';
 }
+
+const addListButton = document.getElementById('addListButton');
+const addListBox = document.getElementById('addListBox');
+
+addListButton.addEventListener('click', () => {
+  addListBox.classList.remove('is-paused');
+  // addListBox.style.height = '500px';
+});
