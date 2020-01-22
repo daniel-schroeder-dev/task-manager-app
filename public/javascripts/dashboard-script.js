e55202e9613aba39468b3477a6928ce559f6c90f
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-22 16:55:58 -0600

    Add ability to close popup window for creating new task list.

--- public/javascripts/dashboard-script.js
+++ public/javascripts/dashboard-script.js
@@ -7,10 +7,19 @@ if (todaysDate < 10) {
   dateSpan.style.marginLeft = '2px';
 }
 
-const addListButton = document.getElementById('addListButton');
 const addListBox = document.getElementById('addListBox');
 
-addListButton.addEventListener('click', () => {
-  addListBox.classList.remove('is-paused');
-  // addListBox.style.height = '500px';
+document.addEventListener('click', (e) => {
+  if (e.target.id === 'addListButton' || e.target.parentElement.id === 'addListButton') {
+    return addListBox.classList.remove('is-paused');
+  }
+  if (e.target.id === 'close' || e.target.classList.contains('btn-close')) {
+    addListBox.classList.remove('fade-in');
+    addListBox.classList.add('fade-out');
+    return setTimeout(() => {
+      addListBox.classList.add('is-paused');
+      addListBox.classList.add('fade-in');
+      addListBox.classList.remove('fade-out');
+    }, 100);
+  }
 });
