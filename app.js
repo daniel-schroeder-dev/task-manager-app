6685b7e19cc237036ce34c322d0bff5d257f9a8d
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-22 18:40:22 -0600

    Change order of routers since I'm using a catch-all in the index router.

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
