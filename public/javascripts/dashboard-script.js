188004f16ae5263bcc72e9f9d48f6c68c5ca764a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-25 01:10:03 -0600

    Add TaskList constructor function and move TaskList creation logic there.

--- public/javascripts/dashboard-script.js
+++ public/javascripts/dashboard-script.js
@@ -1,23 +1,88 @@
-const dateSpan = document.getElementById('date');
+const taskLists = [];
 
-const todaysDate = new Date().getDate();
-dateSpan.textContent = todaysDate;
-
-if (todaysDate < 10) {
-  dateSpan.style.marginLeft = '2px';
-}
-
+const pageTitleElement = document.getElementById('pageTitle');
 const createListInput = document.getElementById('createList');
 const createListsContainer = document.querySelector('.create-lists-container');
+const taskContainer = document.getElementById('taskContainer');
+const createTaskInput = document.getElementById('createTask');
+const addListButton = document.getElementById('addListButton');
 const saveButton = document.querySelector('.btn-save');
 const addListBox = document.getElementById('addListBox');
-const pageTitleElement = document.getElementById('pageTitle');
 const editTaskBox = document.getElementById('editTaskBox');
 
+
+
+const TaskList = function(listName) {
+  
+  this.listName = listName;
+  this.url = '/' + listName.toLowerCase().replace(/\s/gi, '-');
+
+  this.createTaskListDOMElement = function() {
+    const div = document.createElement('div');
+    const i = document.createElement('i');
+    const span = document.createElement('span');
+    const a = document.createElement('a');
+    a.classList.add('task-list-nav');
+    /* TODO: this happens in changePageURL as well, make it DRY */
+    a.href = this.url
+    span.textContent = this.listName;
+    // fix wierd margin collapse when DOM element is added but page isn't reloaded.
+    span.style.marginLeft = '4px' 
+    i.classList.add('fas', 'fa-bars');
+    a.appendChild(i);
+    a.appendChild(span);
+    div.appendChild(a);
+    return div;
+  };
+
+  this.createTaskListDB = async function() {
+    
+    const data = {
+      pageUrl: this.url,
+      name: this.listName,
+    };
+
+    const response = await fetch('/createTaskList', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    const json = await response.json();
+    
+    console.log('Saved taskList to DB: ', json);
+
+  };
+
+};
+
+const setTodaysDate = () => {
+  
+  const dateSpan = document.getElementById('date');
+  const todaysDate = new Date().getDate();
+  
+  dateSpan.textContent = todaysDate;
+
+  if (todaysDate < 10) {
+    dateSpan.style.marginLeft = '2px';
+  }
+
+};
+
 const changePageURL = (pageName) => {
+
   if (window.history.state && window.history.state.pageName === pageName) return;
+  
   const formattedPageName = pageName.toLowerCase().replace(/\s/gi, '-');
   const url = '/' + formattedPageName;
+  
   window.history.replaceState({ pageName }, '', url);
   
 };
@@ -25,65 +90,6 @@ const changePageURL = (pageName) => {
 const updatePageState = (pageName) => {
   pageTitleElement.textContent = pageName;
   pageTitleElement.nextElementSibling.setAttribute('placeholder', `Add Task to "${pageName}"`);
-};
-
-const createTaskListDOMElement = (listName) => {
-  const div = document.createElement('div');
-  const i = document.createElement('i');
-  const span = document.createElement('span');
-  const a = document.createElement('a');
-  a.classList.add('task-list-nav');
-  /* TODO: this happens in changePageURL as well, make it DRY */
-  a.href = '/' + listName.toLowerCase().replace(/\s/gi, '-');
-  span.textContent = listName;
-  // fix wierd margin collapse when DOM element is added but page isn't reloaded.
-  span.style.marginLeft = '4px' 
-  i.classList.add('fas', 'fa-bars');
-  a.appendChild(i);
-  a.appendChild(span);
-  div.appendChild(a);
-  createListsContainer.firstElementChild.after(div);
-};
-
-const createTaskList = async (listName) => {
-  createTaskListDOMElement(listName);
-  
-  const pageUrl = '/' + listName.toLowerCase().replace(/\s/gi, '-');
-  
-  const data = {
-    pageUrl,
-    name: listName,
-  };
-
-  const response = await fetch('/createTaskList', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  const json = await response.json();
-  
-  console.log('Saved taskList to DB: ', json);
-
-};
-
-const removeAddListBox = () => {
-  addListBox.classList.remove('fade-in');
-  addListBox.classList.add('fade-out');
-  setTimeout(() => {
-    addListBox.classList.add('is-paused');
-    addListBox.classList.add('fade-in');
-    addListBox.classList.remove('fade-out');
-    createListInput.value = '';
-    saveButton.setAttribute('disabled', true);
-  }, 100);
 };
 
 const removeBox = (boxID) => {
@@ -100,19 +106,25 @@ const removeBox = (boxID) => {
   }, 100);
 };
 
+addListButton.addEventListener('click', () => {
+  addListBox.style.display = 'block';
+  addListBox.classList.remove('is-paused');
+});
+
 document.addEventListener('click', (e) => {
-  if (e.target.id === 'addListButton' || e.target.parentElement.id === 'addListButton') {
-    addListBox.style.display = 'block';
-    return addListBox.classList.remove('is-paused');
-  }
   if (e.target.id === 'close' || e.target.classList.contains('btn-close')) {
     return removeBox(e.target.getAttribute('data-target'));
   }
   if (e.target.classList.contains('btn-save')) {
-    createTaskList(createListInput.value);
-    changePageURL(createListInput.value);
-    updatePageState(createListInput.value);
-    removeAddListBox();
+    const taskListName = createListInput.value;
+    const taskList = new TaskList(taskListName);
+    taskLists.push(taskList);
+    const taskListDOMElement = taskList.createTaskListDOMElement();
+    createListsContainer.firstElementChild.after(taskListDOMElement);
+    taskList.createTaskListDB();
+    changePageURL(taskListName);
+    updatePageState(taskListName);
+    removeBox('addListBox');
     return;
   }
   if (e.target.classList.contains('ellipsis')) {
@@ -125,21 +137,6 @@ createListInput.addEventListener('keyup', function(e) {
   if (this.value.length) return saveButton.removeAttribute('disabled');
   saveButton.setAttribute('disabled', true);
 });
-
-const createTaskInput = document.getElementById('createTask');
-const taskContainer = document.getElementById('taskContainer');
-
-if (taskContainer.querySelector('li')) {
-  document.querySelector('.site-icon').style.display = 'none';
-}
-
-/*
-<li class="active-task">
-  <i class="far fa-square"></i>
-  <span contenteditable="true">New task</span>
-  <span class="ellipsis">&#8230;</span>
-</li>
-*/
 
 createTaskInput.addEventListener('keydown', function(e) {
   if (e.keyCode !== 13) return;
@@ -184,3 +181,13 @@ taskContainer.addEventListener('keydown', (e) => {
   e.target.blur();
   createTaskInput.focus();
 });
+
+/* Init stuff to run on page load */
+
+// When the page first loads, check to see if we need to wipe the .site-icon from the taskContainer 
+if (taskContainer.querySelector('li')) {
+  document.querySelector('.site-icon').style.display = 'none';
+}
+
+// sets the date in the 'Today' icon
+setTodaysDate();
