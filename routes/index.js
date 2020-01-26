4a3da2fd6d8f8c3214c80af89e21b013f0918a05
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-26 17:13:27 -0600

    Add some guard conditions when loading a taskList at GET /* route-handler.

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,16 @@ router.get('/', function(req, res, next) {
 router.get('/*', auth, async (req, res, next) => {
   const [ taskList ] = await TaskList.find({ url: req.url }).populate('tasks').exec();
   let taskLists = await TaskList.find( { ownerId: req.user._id });
-  taskLists = taskLists.sort((a, b) => {
-    return Date.parse(b.createdAt) - Date.parse(a.createdAt);
-  });
-  taskList.tasks = taskList.tasks.sort((a, b) => {
-    return Date.parse(b.createdAt) - Date.parse(a.createdAt);
-  });
+  if (taskLists.length > 1) {
+    taskLists = taskLists.sort((a, b) => {
+      return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+    });
+  }
+  if (taskList.tasks.length) {
+    taskList.tasks = taskList.tasks.sort((a, b) => {
+      return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+    });
+  }
   res.render('dashboard', { pageTitle: taskList.name, username: req.user.username, taskLists, tasks: taskList.tasks });
 });
 
