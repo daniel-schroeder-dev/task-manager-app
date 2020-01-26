f181171c6a1e76823e066384ec4f2ce6280dbf25
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-26 11:32:06 -0600

    Sort tasks by creation date when loaded from the DB at GET /*.

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,10 @@ router.get('/*', auth, async (req, res, next) => {
   let taskLists = await TaskList.find( { ownerId: req.user._id });
   taskLists = taskLists.sort((a, b) => {
     return Date.parse(b.createdAt) - Date.parse(a.createdAt);
-  })
+  });
+  taskList.tasks = taskList.tasks.sort((a, b) => {
+    return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+  });
   res.render('dashboard', { pageTitle: taskList.name, username: req.user.username, taskLists, tasks: taskList.tasks });
 });
 
