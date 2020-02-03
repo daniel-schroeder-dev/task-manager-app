a7e8adae7663012e4bd8ae278f864fd30781d54a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-02-03 15:44:49 -0600

    Make hideDialogBox a method of DialogBox.

--- public/javascripts/dashboard-script.js
+++ public/javascripts/dashboard-script.js
@@ -37,6 +37,49 @@ function DialogBox(element) {
   this.element = element;
 };
 
+/*
+*   Removes the box with boxID from the DOM and resets the state of the box.
+*/
+DialogBox.prototype.hideDialogBox = function() {
+  
+  this.element.classList.remove('fade-in');
+  this.element.classList.add('fade-out');
+  
+  /*
+
+  *** RESEARCH ***
+  
+  I honestly don't understand why I need this setTimeout() call to get this 
+  fade-in/fade-out animation to work, but it was the only thing I tried that 
+  worked so I'm using it. I'm not sure if the issue is the animation itself, 
+  or the way in which I'm triggering it. I have a vague understanding that 
+  setTimeout() will run this code in a different portion of the event loop, 
+  but I really need to understand this concept deeply if I'm going to use it 
+  in my code.
+
+  */
+
+  /* 
+  *   This is neccessary to give the fade-out animation time to run before 
+  *   adding back the .fade-in and .is-paused classes.
+  */
+  setTimeout(() => {
+
+    this.element.classList.add('is-paused');
+    this.element.classList.add('fade-in');
+    this.element.classList.remove('fade-out');
+    
+    this.element.style.display = 'none';
+    
+    if (this.element.id === 'addListBox') {
+      createListInput.value = '';
+      saveListButton.setAttribute('disabled', true);
+    }
+    
+  }, 100);
+
+};
+
 /********************/
 /******* Task *******/
 /********************/
@@ -370,50 +413,6 @@ const changePageURL = (newActiveTaskList) => {
 
   window.history.replaceState({ taskListName: activeTaskList.name }, '', url);
   
-};
-
-/*
-*   Removes the box with boxID from the DOM and resets the state of the box.
-*/
-const hideDialogBox = (boxID) => {
-  
-  const box = document.getElementById(boxID);
-  
-  box.classList.remove('fade-in');
-  box.classList.add('fade-out');
-  
-  /*
-
-  *** RESEARCH ***
-  
-  I honestly don't understand why I need this setTimeout() call to get this 
-  fade-in/fade-out animation to work, but it was the only thing I tried that 
-  worked so I'm using it. I'm not sure if the issue is the animation itself, 
-  or the way in which I'm triggering it. I have a vague understanding that 
-  setTimeout() will run this code in a different portion of the event loop, 
-  but I really need to understand this concept deeply if I'm going to use it 
-  in my code.
-
-  */
-
-  /* 
-  *   This is neccessary to give the fade-out animation time to run before 
-  *   adding back the .fade-in and .is-paused classes.
-  */
-  setTimeout(() => {
-
-    box.classList.add('is-paused');
-    box.classList.add('fade-in');
-    box.classList.remove('fade-out');
-    
-    box.style.display = 'none';
-    
-    if (boxID === 'addListBox') createListInput.value = '';
-    
-    saveListButton.setAttribute('disabled', true);
-
-  }, 100);
-
 };
 
 /*
