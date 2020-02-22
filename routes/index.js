559d6ddf88b90ff93dcca948c476591fef260767
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-02-21 19:14:10 -0600

    Only show numTasks for incompleteTasks in taskListNavElements on page load.

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res, next) {
 router.get('/*', auth, async (req, res, next) => {
   console.log('\n\n\n\n\n\n\n\n\n\n');
   const defaultTaskLists = [ 'Inbox', 'Today', 'Week', 'Trash', 'Completed' ];
+  const findIncompleteTasks = (taskListName) => taskLists.find(taskList => taskList.name === taskListName).tasks.filter(task => !task.completed);
   const taskList = await TaskList.findOne({ url: req.url }).populate('tasks').exec();
   let taskLists = await TaskList.find({ ownerId: req.user._id }).populate('tasks').exec();
   if (taskLists.length > 1) {
@@ -23,7 +24,7 @@ router.get('/*', auth, async (req, res, next) => {
       return Date.parse(b.createdAt) - Date.parse(a.createdAt);
     });
   }
-  res.render('dashboard', { pageTitle: taskList.name, username: req.user.username, taskLists, tasks: taskList.tasks, defaultTaskLists });
+  res.render('dashboard', { pageTitle: taskList.name, username: req.user.username, taskLists, tasks: taskList.tasks, defaultTaskLists, findIncompleteTasks });
 });
 
 module.exports = router;
