f1a9dd1c2e02212e35cbbdb024090d118055f8ab
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-02-21 19:32:26 -0600

    Ensure that the number of tasks in the completedTaskListNavElement is updated when the completed status of a task is toggled.

--- public/javascripts/modules/helpers/toggle-completed-status.mjs
+++ public/javascripts/modules/helpers/toggle-completed-status.mjs
@@ -29,6 +29,7 @@ const toggleCompletedStatus = (checkbox, activeTaskList, completedTaskList, inco
   }
 
   activeTaskList.navElement.querySelector('.num-tasks').textContent = activeTaskList.numIncompleteTasks() || '';
+  completedTaskList.navElement.querySelector('.num-tasks').textContent = completedTaskList.tasks.length || '';
 
 };
 
